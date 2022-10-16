648e5b2d1598c4fdbc1b167219a2b9992f4ee134
Kevin Brechbühl <mail@kevinbrechbuehl.ch>
2022-10-16 21:50:39 +0200

    Add Serilog with File Sink

--- Serilog.Demo/Program.cs
+++ Serilog.Demo/Program.cs
@@ -1,8 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Logging.AddSerilog(logger);
 
 var app = builder.Build();
 app.MapControllers();
